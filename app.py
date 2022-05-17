d30c8f70540c3f0340c83fc53018e299a53a46f3
Eburonia <maurice.vossen@planet.nl>
2022-05-17 19:30:12 +0000

    update

--- app.py
+++ app.py
@@ -17,12 +17,27 @@ def energielabel():
 
     if request.method == "POST":
 
-        electriciteitsverbruik = request.form.get("electriciteitsverbruik")
-        electriciteitteruggeleverd = request.form.get("electriciteitteruggeleverd")
-        gasverbruik = request.form.get("gasverbruik")
+        electriciteitsverbruik = request.form.get("electriciteitsverbruik") #E8
+        electriciteitteruggeleverd = request.form.get("electriciteitteruggeleverd") #E9
+        gasverbruik = request.form.get("gasverbruik") #E10
         soorthuis = request.form.get("soorthuis")
-        volumehuis = request.form.get("volumehuis")
-        aantalbewoners = request.form.get("aantalbewoners")
+        volumehuis = request.form.get("volumehuis") #E12
+        aantalbewoners = request.form.get("aantalbewoners") #E13
+
+        soorthuis_pct = None
+
+        if soorthuis == 'vrijstaand':
+            soorthuis_pct = 0.5
+
+        elif soorthuis == 'halfvrijstaand':
+            soorthuis_pct = 0.25
+
+        elif soorthuis == 'tussenliggend':
+            soorthuis_pct = 0
+
+        energieverbruik = (((float(electriciteitsverbruik)-float(electriciteitteruggeleverd)) * 3600 + float(gasverbruik) * 0.833 * 50000) * (1 - soorthuis_pct) / float(volumehuis)) * 0.8 + (((float(electriciteitsverbruik) - float(electriciteitteruggeleverd)) * 3600 + float(gasverbruik) * 0.833 * 50000) * (1 - soorthuis_pct) / float(volumehuis)) * 0.2 / float(aantalbewoners)
+
+        energieverbruik = int(round(energieverbruik, 0))
 
         soorthuis1 = None
         soorthuis2 = None
@@ -38,6 +53,9 @@ def energielabel():
         selected8 = None
         selected9 = None
         selected10 = None
+
+        energielabel = None
+        energielabel_color = None
 
         if soorthuis == 'vrijstaand':
             soorthuis1 = 'selected'
@@ -78,6 +96,51 @@ def energielabel():
         elif int(aantalbewoners) == 10:
             selected10 = 'selected'
 
+
+        if energieverbruik < 35000:
+            energielabel = 'A++'
+            energielabel_color = ''
+
+        elif energieverbruik >= 35000:
+            energielabel = 'A+'
+            energielabel_color = ''
+
+        elif energieverbruik >= 45000:
+            energielabel = 'A'
+            energielabel_color = ''
+
+        elif energieverbruik >= 55000:
+            energielabel = 'B++'
+            energielabel_color = ''
+
+        elif energieverbruik >= 65000:
+            energielabel = 'B+'
+            energielabel_color = ''
+
+        elif energieverbruik >= 75000:
+            energielabel = 'B'
+            energielabel_color = ''
+
+        elif energieverbruik >= 85000:
+            energielabel = 'C'
+            energielabel_color = ''
+
+        elif energieverbruik >= 95000:
+            energielabel = 'D'
+            energielabel_color = ''
+
+        elif energieverbruik >= 105000:
+            energielabel = 'E'
+            energielabel_color = ''
+
+        elif energieverbruik >= 115000:
+            energielabel = 'F'
+            energielabel_color = ''
+
+        elif energieverbruik >= 125000:
+            energielabel = 'G'
+            energielabel_color = ''
+
         return render_template("index.html",
                                electriciteitsverbruik=electriciteitsverbruik,
                                electriciteitteruggeleverd=electriciteitteruggeleverd,
@@ -97,7 +160,9 @@ def energielabel():
                                selected10=selected10,
                                soorthuis1=soorthuis1,
                                soorthuis2=soorthuis2,
-                               soorthuis3=soorthuis3)
+                               soorthuis3=soorthuis3,
+                               energieverbruik=energieverbruik,
+                               energielabel=energielabel)
 
     return render_template("index.html",
                                electriciteitsverbruik=electriciteitsverbruik,
